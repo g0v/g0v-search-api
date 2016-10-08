468bf46e1b2be0849847d690d6f8a46e755f3b71
Ronny Wang <ronnywang@gmail.com>
2016-10-08 14:33:23 +0800

    加上可以支援複雜的 json query

--- query.php
+++ query.php
@@ -2,6 +2,7 @@
 
 $url = getenv('SEARCH_URL') . '/entry/_search';
 $terms = array();
+
 foreach (array('q') as $k) {
     if (array_key_exists($k, $_GET)) {
         $terms[] = urlencode($k) . '=' . urlencode($_GET[$k]);
@@ -17,4 +18,8 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET');
 
 $curl = curl_init($url);
+if (array_key_exists('query', $_GET)) {
+    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
+    curl_setopt($curl, CURLOPT_POSTFIELDS, strval($_GET['query']));
+}
 curl_exec($curl);
