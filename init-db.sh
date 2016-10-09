1d08db9b0d1d1bfbdcacb150dba6e2fd28b3bda9
Ronny Wang <ronnywang@gmail.com>
2016-10-09 11:36:39 +0800

    加上 url 和 updated_at

--- init-db.sh
+++ init-db.sh
@@ -8,6 +8,8 @@ curl -XPUT ${SEARCH_URL}/entry/_mapping -d '
  "entry" : {
   "date_detection": false,
   "properties" : {
+    "url": {"type" : "string", "index": "not_analyzed"},
+    "updated_at": {"type": "integer"},
     "title" : {"type" : "string", "store" : false, "index" : "analyzed", "analyzer" : "cjk" },
     "content" : {"type" : "string", "store" : false, "index" : "analyzed", "analyzer" : "cjk" }
   }
